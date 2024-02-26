2a84c55acfaaffff9a7062ac40a643a90a076c21
SIMULATAN <simulatan2319@gmail.com>
2024-02-26 12:11:22 +0100

    Create shop overview

---
+++ src/app/item-card/item-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Item } from "../shop-model";
+
+@Component({
+  selector: 'app-item-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-card.component.html',
+  styleUrl: './item-card.component.css'
+})
+export class ItemCardComponent {
+  @Input() item! : Item;
+}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ItemsService } from "../items.service";
+import { Item } from "../shop-model";
+import { ItemCardComponent } from "../item-card/item-card.component";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [
+    ItemCardComponent,
+    NgForOf
+  ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  items: Item[] = [];
+  itemsService: ItemsService = inject(ItemsService);
 
+  constructor() {
+    this.items = this.itemsService.getItems();
+  }
 }
